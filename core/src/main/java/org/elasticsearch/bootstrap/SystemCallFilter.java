f779a8569928933d25ff2fa44167f45f6b7a6ba3
utzb <utz.bacher@de.ibm.com>
2017-11-17 22:28:07 +0100

    Add s390x to system call filtering
    Add s390x system call data.

--- core/src/main/java/org/elasticsearch/bootstrap/SystemCallFilter.java
+++ core/src/main/java/org/elasticsearch/bootstrap/SystemCallFilter.java
@@ -242,6 +242,7 @@ final class SystemCallFilter {
         Map<String,Arch> m = new HashMap<>();
         m.put("amd64", new Arch(0xC000003E, 0x3FFFFFFF, 57, 58, 59, 322, 317));
         m.put("aarch64",  new Arch(0xC00000B7, 0xFFFFFFFF, 1079, 1071, 221, 281, 277));
+        m.put("s390x", new Arch(0x80000016, 0xFFFFFFFF, 1, 190, 11, 354, 348));
         ARCHITECTURES = Collections.unmodifiableMap(m);
     }
 
